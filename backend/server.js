d39bbc7071c469f3e482565d9e1fe76c3e8c12ab
Akanksha <akankshajha45@gmail.com>
2020-05-14 11:39:54 +0530

    'Project'

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require ('mongoose');
+
+require ('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use (express.json());
+
+
+mongoose.connect('mongodb+srv://Akanksha12:Akanksha12@cluster0-kirnc.gcp.mongodb.net/test?retryWrites=true&w=majority', 
+    {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+    console.log("connected to db")
+}).catch(function(){
+    console.log("not able to connect to db")
+})
+
+const exerciseRouter = require ('./routes/exercises')
+const usersRouter = require('./routes/users')   
+
+app.use('./exercises', exerciseRouter)
+app.use('./users', usersRouter)
+
+
+app.listen(port , () => { console.log ('Server is running at : ',port);
+})
